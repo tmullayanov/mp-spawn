eaad8b3f612a3361c52e256a73c5a15b9147c16b
Timur Mullayanov <TimurM@bitbucket.org>
2017-02-19 17:33:54 +0300

    added sign checking for command-line argument

--- spawn_types.py
+++ spawn_types.py
@@ -6,12 +6,15 @@ Types defined in this module:
 PrecisionAction(argparse.Action)
 '''
 import argparse
-import loggings
+import logging
 
 class PrecisionAction(argparse.Action):
     '''
     Special type for parsing precision of calculation out of cmd args
     '''
+    __default__ = 1000
+    __negative_prec_warn__ = 'precision cannot be negative({}), using default value of {}'
+
     def __init__(self, option_strings, dest, nargs=None, **kwargs):
         if nargs is not None:
             raise ValueError('nargs is not allowed')
@@ -19,5 +22,7 @@ class PrecisionAction(argparse.Action):
 
     def __call__(self, parser, namespace, values, option_strings=None):
         logging.debug('values={}'.format(values))
-        logging.debug('type(values)={}'.format(type(values)))
+        if values < 0:
+            logging.warning(__negative_prec_warn__.format(values, __default__))
+            values = __default__
         setattr(namespace, self.dest, values)
